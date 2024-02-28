73d33c166309749384be894cca21a801a6338091
martirm <martirm@physik.hu-berlin.de>
2024-02-28 10:57:35 +0100

    Added full and fast support for OMP5 in 3d case

--- src/special_functions/spherical_harmonics.f90
+++ src/special_functions/spherical_harmonics.f90
@@ -142,11 +142,11 @@ contains
 
       ! This is a small calculation so it is done with a hand-made definition of matmul so the GPU kernel can be built
       ! clm = Ylm**H \cdot f
-      !$omp do private(i) 
+      !$omp distribute parallel do private(i) 
       do i = 1, n
           clm(i) = dot_product(ylm(:,i),fw(:))
       end do
-      !$omp end do
+      !$omp end distribute parallel do
 
       return
 
